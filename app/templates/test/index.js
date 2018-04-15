5705ca0d2dfc659d5e54590b4632080342cc92dd
m31271n <m31271n@users.noreply.github.com>
2018-04-15 22:54:24 +0800

    feat: add browser-env for testing

--- app/templates/test/index.js
+++ app/templates/test/index.js
@@ -2,10 +2,12 @@ import test from 'ava';
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import browserEnv from 'browser-env';
 
-import <%= camelComponentName %> from '../src/index.jsx';
+import <%= camelComponentName %> from '../src';
 
 configure({ adapter: new Adapter() });
+browserEnv();
 
 test('general usage', t => {
   const msg="testing message";
